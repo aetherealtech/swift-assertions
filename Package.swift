651337bab5f9911e083cf445a5bbab4bfc753395
Dan Coleman <dan@aetherealtech.com>
2024-09-21 11:50:31 -0500

    Update to Swift 6

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.8
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -18,36 +18,18 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "1.1.0")
+        .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "1.3.3")
     ],
     targets: [
         .target(
             name: "Assertions",
             dependencies: [
                 .product(name: "CustomDump", package: "swift-custom-dump")
-            ],
-            swiftSettings: [.concurrencyChecking(.complete)]
+            ]
         ),
         .testTarget(
             name: "AssertionsTests",
-            dependencies: ["Assertions"],
-            swiftSettings: [.concurrencyChecking(.complete)]
-        ),
+            dependencies: ["Assertions"]
+        )
     ]
 )
-
-extension SwiftSetting {
-    enum ConcurrencyChecking: String {
-        case complete
-        case minimal
-        case targeted
-    }
-    
-    static func concurrencyChecking(_ setting: ConcurrencyChecking = .minimal) -> Self {
-        unsafeFlags([
-            "-Xfrontend", "-strict-concurrency=\(setting)",
-            "-Xfrontend", "-warn-concurrency",
-            "-Xfrontend", "-enable-actor-data-race-checks",
-        ])
-    }
-}
